667fd8e7f12dcecda88ec4c94811bede35a685c8
44mira <legolaslada@gmail.com>
2024-04-28 04:01:46 +0800

    feat: integration with database

--- components/EventTable.vue
+++ components/EventTable.vue
@@ -1,7 +1,16 @@
 <script setup>
 import { ref } from "vue";
 
-const eventKeys = ["id", "name", "desc", "status"];
+const eventKeys = [
+  "event id",
+  "admin id",
+  "name",
+  "event time",
+  "orientation time",
+  "location",
+  "description",
+  "status",
+];
 
 const { route, displayedEvents } = defineProps(["route", "displayedEvents"]);
 
@@ -31,13 +40,14 @@ function displayModal(index) {
           @click="displayModal(index)"
           :key="event.id"
         >
-          <th>{{ index + 1 }}</th>
-          <td>{{ event.id }}</td>
+          <td>{{ event.event_id }}</td>
+          <td>{{ event.admin_id }}</td>
           <td>{{ event.name }}</td>
-          <td>{{ event.desc }}</td>
-          <td>
-            <StatusBadge :eventStatus="event.status" />
-          </td>
+          <td>{{ event.event_time }}</td>
+          <td>{{ event.orientation_time }}</td>
+          <td>{{ event.location }}</td>
+          <td>{{ event.description }}</td>
+          <td><StatusBadge :eventStatus="event.status" /></td>
 
           <dialog
             class="modal modal-bottom sm:modal-middle"
@@ -45,11 +55,19 @@ function displayModal(index) {
           >
             <div class="modal-box">
               <figure><img src="/event-placeholder.svg" /></figure>
+              <div class="divider"></div>
               <h3 class="font-bold text-lg">
                 {{ event.name }}
                 <StatusBadge :eventStatus="event.status" />
               </h3>
-              <p class="py-4">{{ event.desc }}</p>
+              <p class="py-4">{{ event.description }}</p>
+              <div class="divider"></div>
+              <div class="text-sm grid grid-cols-2">
+                <p>Orientation:</p>
+                <pre>{{ event.orientation_time.split("T").join(" | ") }}</pre>
+                <p>Event:</p>
+                <pre>{{ event.event_time.split("T").join(" | ") }}</pre>
+              </div>
             </div>
             <form method="dialog" class="modal-backdrop">Close</form>
           </dialog>
--- pages/dashboard/[tab].vue
+++ pages/dashboard/[tab].vue
@@ -1,32 +1,34 @@
 <script setup lang="ts">
+import { createClient } from "@supabase/supabase-js";
+const config = useRuntimeConfig().public;
+
 definePageMeta({
   layout: "dashboard",
 });
 
+const supabase = createClient(config.supabaseUrl, config.supabaseKey);
+
 const route = useRoute().params.tab;
+const events = ref([]);
 
-// dummy
-const events: EventData[] = [
-  { id: 1, name: "Hugyaw", desc: "lorem ipsum dolor sit", status: "active" },
-  { id: 2, name: "Dula", desc: "lorem ipsum", status: "active" },
-  { id: 3, name: "Web3 comes to UP", desc: "tester tester", status: "active" },
-  { id: 4, name: "Hampang", status: "upcoming" },
-  { id: 5, name: "Dula-dula", desc: "testiingg", status: "upcoming" },
-  { id: 6, name: "Mugna Workshop", status: "past" },
-];
+async function getEvents() {
+  const { data } = await supabase
+    .from("events")
+    .select("*")
+    .eq("status", route);
+  events.value = data;
+}
 
-const displayedEvents = computed(() => {
-  return events.filter((event) => route === event.status);
+onMounted(() => {
+  getEvents();
 });
 </script>
 
 <template>
   <div class="w-full bg-base flex flex-col items-center">
-    <div
-      class="carousel carousel-center p-4 space-x-4 bg-base w-96 rounded-box"
-    >
+    <div class="carousel carousel-center space-x-4 bg-base w-fit">
       <div
-        v-for="(event, index) in displayedEvents"
+        v-for="(event, index) in events"
         class="carousel-item card w-96 rounded-xl"
         :key="event.id"
         :id="`item-${index}`"
@@ -37,20 +39,27 @@ const displayedEvents = computed(() => {
             {{ event.name }}
             <StatusBadge :eventStatus="event.status" />
           </h2>
-          <p>{{ event.desc }}</p>
+          <p>{{ event.description }}</p>
+          <div class="divider"></div>
+          <div class="text-sm grid grid-cols-2">
+            <p>Orientation:</p>
+            <pre>{{ event.orientation_time.split("T").join(" | ") }}</pre>
+            <p>Event:</p>
+            <pre>{{ event.event_time.split("T").join(" | ") }}</pre>
+          </div>
         </div>
       </div>
     </div>
 
     <div class="flex justify-center w-full py-2 gap-2">
       <a
-        v-for="index in displayedEvents.length"
+        v-for="index in events.length"
         class="btn btn-xs bg-secondary text-white"
         :href="`#item-${index - 1}`"
       >
         {{ index }}
       </a>
     </div>
-    <EventTable :route="route" :displayedEvents="displayedEvents" />
+    <EventTable :route="route" :displayedEvents="events" />
   </div>
 </template>
